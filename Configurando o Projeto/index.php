16b011b4cc2842a347e29e208f0baef2cc212636
Marcos Paulo <marcosmp1516@gmail.com>
2018-06-12 23:12:50 -0300

    criar a class page

--- Configurando o Projeto/index.php
+++ Configurando o Projeto/index.php
@@ -2,19 +2,18 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
 
-    $Sql = new Hcode\DB\sql();
-    $results = $Sql->select("SELECT * FROM tb_users");
-
-    echo json_encode($results);
-
+    $page = new Page();
+    $page->setTpl("index");
 });
 
 $app->run();
-
+//www.hcodecommerce.com.br
  ?>
