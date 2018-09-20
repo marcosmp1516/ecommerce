9a8e3befa86f6860c60d1c73a85ee09d4efc522f
Marcos Paulo <marcosmp1516@gmail.com>
2018-09-20 00:10:57 -0300

    pasinacao

--- Configurando o Projeto/vendor/hcodebr/php-classes/src/Model/Category.php
+++ Configurando o Projeto/vendor/hcodebr/php-classes/src/Model/Category.php
@@ -98,6 +98,31 @@ public function getProducts($related = true)
   }
 }
 
+public function getProductPage($page = 1, $itemsPerPage = 3)
+{
+    $start = ($page - 1) * $itemsPerPage;
+
+    $sql = new Sql();
+
+  $resultProd = $sql->select(" SELECT SQL_CALC_FOUND_ROWS *
+        FROM tb_products a
+        INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
+        INNER JOIN tb_categories c ON c.idcategory = b.idcategory
+        WHERE c.idcategory = :idcategory
+        LIMIT $start, $itemsPerPage;
+      ", [
+          ':idcategory'=>$this->getidcategory()
+      ]);
+
+  $resultTotal = $sql->select("SELECT FOUND_ROWS() AS ntotal;");
+
+  return [
+      'data'=>Products::checkList($resultProd),
+      'total'=>(int)$resultTotal[0]["ntotal"],
+      'pages'=>ceil($resultTotal[0]["ntotal"] / $itemsPerPage)
+  ];
+}
+
 public function addProduct(Products $product)
 {
   $sql = new Sql();
